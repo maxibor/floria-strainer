d625e210006c34cddaee781585541139ce7bb66d
maxibor <maxime.borry@gmail.com>
2024-04-04 14:19:18 +0200

    feat: set logging level to info

--- floria_strainer/__init__.py
+++ floria_strainer/__init__.py
@@ -1,4 +1,8 @@
 from importlib import metadata
+import logging
 
 __version__ = metadata.metadata("floria-strainer")["Version"]
 __author__ = metadata.metadata("floria-strainer")["Author"]
+
+logging.basicConfig(level=logging.INFO, format="%(levelname)s - %(message)s")
+logger = logging.getLogger(__name__)
